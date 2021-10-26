800c2b345fd19fcd25585fca66c1cddd104badbe
aapxx87 <57785764+aapxx87@users.noreply.github.com>
2021-10-26 10:57:27 +0300

    update

--- scripts/script.js
+++ scripts/script.js
@@ -215,8 +215,8 @@ class Users {
 
 
 // создаем инстансы двух предустановленных юзеров
-const newUser = new Users('tolik', 1987)
-const newUser2 = new Users('katya', 1987)
+const newUser = new Users('Tolik', 1987)
+const newUser2 = new Users('Katya', 1987)
 
 // добавляем предустановленных юзеров (пушим) в инстанс с основной датой прилы (в массив)
 appData.addNewUser(newUser)
